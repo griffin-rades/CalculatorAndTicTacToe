c9d20eed52c71e150faa681d79683967279f32c4
Griffin Rades <ggrade16@smumn.edu>
2019-10-25 15:31:45 -0500

    progress

--- Calculator/Calculator/Controllers/CalcViewController.swift
+++ Calculator/Calculator/Controllers/CalcViewController.swift
@@ -21,32 +21,31 @@ class CalcViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
-        createButton()
-        distance = 80
-        createButton()
-        //printArray()
     }
     
-    
-    func createButton(){
-        button = UIButton()
-        button.setTitleColor(.red, for: .normal)
-        button.backgroundColor = .white
-        button.setTitle(" " + buttonNames[0][0] + " ", for: .normal)
-        
-        view.addSubview(button)
-        setButtonContraints()
-    }
-    
-    func setButtonContraints(){
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: CGFloat(distance)).isActive = true
-        //button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+}
 
-    }
-    
+
+
+//Testing button creation and loactions.
+//    func createButton(){
+//        button = UIButton()
+//        button.setTitleColor(.red, for: .normal)
+//        button.backgroundColor = .white
+//        button.setTitle(" " + buttonNames[0][0] + " ", for: .normal)
+//
+//        view.addSubview(button)
+//        setButtonContraints()
+//    }
+//
+//    func setButtonContraints(){
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: CGFloat(distance)).isActive = true
+//        //button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+//        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//
+//    }
 //    func printArray(){
 //        for var k in 0..<buttonNames.count{
 //            for var x in 0..<buttonNames[k].count{
@@ -54,5 +53,5 @@ class CalcViewController: UIViewController {
 //            }
 //        }
 //    }
-}
+
 
