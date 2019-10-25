eb28219dff29d9b16933909c2377cf2e06fff22e
Griffin Rades <ggrade16@smumn.edu>
2019-10-25 15:29:43 -0500

    Have an idea on how to layout buttons.

--- Calculator/Calculator/Controllers/CalcViewController.swift
+++ Calculator/Calculator/Controllers/CalcViewController.swift
@@ -9,18 +9,43 @@
 import UIKit
 
 class CalcViewController: UIViewController {
-    
+    var distance = 20
     var button:UIButton!
-    var buttonNames: [[String]] = [["Clear","Negate","%","/"],["7","8","9","X"],["4","5","6","-"],["1","2","3","+"],["0",".","="]]
+    var buttonNames: [[String]] = [["Clear","Negate","%","/"],
+                                   ["7","8","9","X"],
+                                   ["4","5","6","-"],
+                                   ["1","2","3","+"],
+                                   ["0",".","="]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
         createButton()
+        distance = 80
+        createButton()
         //printArray()
     }
     
+    
+    func createButton(){
+        button = UIButton()
+        button.setTitleColor(.red, for: .normal)
+        button.backgroundColor = .white
+        button.setTitle(" " + buttonNames[0][0] + " ", for: .normal)
+        
+        view.addSubview(button)
+        setButtonContraints()
+    }
+    
+    func setButtonContraints(){
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: CGFloat(distance)).isActive = true
+        //button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+    }
     
 //    func printArray(){
 //        for var k in 0..<buttonNames.count{
@@ -29,22 +54,5 @@ class CalcViewController: UIViewController {
 //            }
 //        }
 //    }
-    
-    func createButton(){
-        button = UIButton()
-        button.setTitleColor(.red, for: .normal)
-        button.backgroundColor = .white
-        button.setTitle("Click ME", for: .normal)
-        
-        view.addSubview(button)
-        setButtonContraints()
-    }
-    func setButtonContraints(){
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    }
 }
 
