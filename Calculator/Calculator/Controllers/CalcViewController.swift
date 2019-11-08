825d16e14385e9cca7bfae288c1f2f4dcbd3e7de
Griffin Rades <ggrade16@smumn.edu>
2019-11-08 14:27:20 -0600

    Working app. However, The constraints are not working correctly. They are stacking up.

--- Calculator/Calculator/Controllers/CalcViewController.swift
+++ Calculator/Calculator/Controllers/CalcViewController.swift
@@ -13,11 +13,13 @@ class CalcViewController: UIViewController {
     var buttonDictionary = [String:Any]()
     var calculatorLabel = UILabel()
     var calculatorButtonsArray: [CalcButtons] = []
+    var extraCalculatorButtons: [CalcButtons] = []
     var currentScreenNumber = 0.0
     var previousScreenNumber = 0.0
     var totalValue = 0.0
     var operation = ""
     let mainButtonView = UIView()
+    let extraButtonsView = UIView()
     var viewDictionary = [String:Any]()
     
     /*
@@ -29,10 +31,11 @@ class CalcViewController: UIViewController {
         
         let buttonNumbers = ["Clear","0","1","2","3","4","5","6","7","8","9"] //Array of the number button label names including clear and negate
         let operationButtons = ["X","%","/","+","-",".","=","+/-"] //Array of operation button label names
+        let landScapeButtons = ["(",")","mc","m+","m-","mr","2nd","x^2","x^3","x^y","e^x","10^x","1/x","1/x^-2","1/x^-3","1/x^-y","ln","log","x!","sin","cos","tan","e","EE","Rad","sinh","cosh","tanh","pi","Rans"]
         
         //set up the calculator label
-        self.calculatorLabel.backgroundColor = .white
-        self.calculatorLabel.text = "0"
+        self.calculatorLabel.backgroundColor = .blue
+        self.calculatorLabel.text = ""
         self.calculatorLabel.textAlignment = .right
         self.calculatorLabel.font = self.calculatorLabel.font.withSize(40)
         
@@ -67,7 +70,23 @@ class CalcViewController: UIViewController {
             self.calculatorButtonsArray.append(x)
             self.buttonDictionary["SC" + String(i)] = x.calculatorButton
         }
-        addButtonConstraints() //add constraints to the buttons using the button dicitonary
+        for i in 0..<landScapeButtons.count{
+            let x = CalcButtons()
+            
+            x.buttonLabel = landScapeButtons[i]
+            x.calculatorButton = UIButton(type: .system)
+            x.calculatorButton?.setTitle(x.buttonLabel, for: .normal)
+            x.calculatorButton?.backgroundColor = .gray
+            x.calculatorButton?.setTitleColor(.white, for: .normal)
+            x.calculatorButton?.addTarget(self, action: #selector(operationClicked), for: .touchUpInside)
+            x.calculatorButton?.titleLabel?.font = self.calculatorLabel.font
+            
+            self.extraCalculatorButtons.append(x)
+            self.buttonDictionary["EB" + String(i)] = x.calculatorButton
+        }
+        createMainButtonView(isPortrait: true)
+//        addStandardButtonConstraints() //add constraints to the buttons using the button dicitonary
+//        addExtraButtonConstraints()
     }
     
     override func viewDidLoad() {
@@ -75,6 +94,16 @@ class CalcViewController: UIViewController {
 
         self.view.backgroundColor = .white
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        if UIDevice.current.orientation.isLandscape {
+            createMainButtonView(isPortrait: false)
+        } else {
+            createMainButtonView(isPortrait: true)
+        }
+    }
+
     /*
      MARK: listener functions to listen for when a button is clicked
      */
@@ -170,28 +199,69 @@ class CalcViewController: UIViewController {
         }
     }
     /*
-     MARK: addconstraints to buttons created in loadView
+     MARK: Create the view to contain the sets of buttons.
      */
-    func addButtonConstraints(){
+    func createMainButtonView(isPortrait:Bool){
         self.view.addSubview(self.calculatorLabel) //add the label to the screen
-        self.view.addSubview(self.mainButtonView)
+        self.view.addSubview(self.extraButtonsView)
         
         self.calculatorLabel.translatesAutoresizingMaskIntoConstraints = false //remove auto constraints
         self.mainButtonView.translatesAutoresizingMaskIntoConstraints = false
+        self.extraButtonsView.translatesAutoresizingMaskIntoConstraints = false
         
         self.mainButtonView.backgroundColor = .black
+        self.extraButtonsView.backgroundColor = .red
         self.viewDictionary["buttonView"] = self.mainButtonView
         self.viewDictionary["label"] = self.calculatorLabel
+        self.viewDictionary["otherButtons"]  = self.extraButtonsView
         
-        let viewConstraint0 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[label]-5-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
-        let viewConstraint1 = NSLayoutConstraint.constraints(withVisualFormat: "H:|[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
-        let viewConstraint2 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label(200)]-0-[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
-        
-        self.view.addConstraints(viewConstraint0)
-        self.view.addConstraints(viewConstraint1)
-        self.view.addConstraints(viewConstraint2)
-        
-        
+//        let pViewConstraint0 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[label]-5-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+//        let pViewConstraint1 = NSLayoutConstraint.constraints(withVisualFormat: "H:|[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+//        let pViewConstraint2 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label(200)]-0-[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+
+//        let lViewConstraint0 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[label]-5-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+//        let lViewConstraint1 = NSLayoutConstraint.constraints(withVisualFormat: "H:|[otherButtons(200)]-0-[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+//        let lViewConstraint2 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label]-0-[otherButtons]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+
+        if isPortrait{
+            print("Port")
+            self.mainButtonView.removeFromSuperview()
+            self.extraButtonsView.removeFromSuperview()
+            
+            self.view.addSubview(self.mainButtonView)
+            let pViewConstraint0 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[label]-5-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+            let pViewConstraint1 = NSLayoutConstraint.constraints(withVisualFormat: "H:|[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+            let pViewConstraint2 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label(100)]-0-[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+            
+            self.view.addConstraints(pViewConstraint0)
+            self.view.addConstraints(pViewConstraint1)
+            self.view.addConstraints(pViewConstraint2)
+            
+            addStandardButtonConstraints()
+        }else{
+            print("Land")
+            self.mainButtonView.removeFromSuperview()
+            self.view.addSubview(self.mainButtonView)
+            self.view.addSubview(self.extraButtonsView)
+            
+            let lViewConstraint0 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[label]-5-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+            let lViewConstraint1 = NSLayoutConstraint.constraints(withVisualFormat: "H:|[otherButtons(400)]-0-[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+            let lViewConstraint2 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label]-0-[otherButtons]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+            let lViewConstraint3 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label]-0-[buttonView]|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.viewDictionary)
+
+            self.view.addConstraints(lViewConstraint0)
+            self.view.addConstraints(lViewConstraint1)
+            self.view.addConstraints(lViewConstraint2)
+            self.view.addConstraints(lViewConstraint3)
+            
+            addStandardButtonConstraints()
+            addExtraButtonConstraints()
+        }
+    }
+    /*
+     MARK: Standard buttons constrained within mainButtonView
+     */
+    func addStandardButtonConstraints(){
         for i in calculatorButtonsArray{
             self.mainButtonView.addSubview(i.calculatorButton!) //add each button in the button array to the view
             i.calculatorButton!.translatesAutoresizingMaskIntoConstraints = false //remove auto constraints for each button in the button array
@@ -213,7 +283,7 @@ class CalcViewController: UIViewController {
         let buttonConstraintH_6 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-[n0]-[SC5(==nClear)]-[SC6(==nClear)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
 
 
-        //add each of the constaints to the view
+        //add each of the constaints to the mainButtonView
         //self.view.addConstraints(buttonConstraint_1)
         self.mainButtonView.addConstraints(buttonConstraint_2)
         self.mainButtonView.addConstraints(buttonConstraint_3)
@@ -225,5 +295,36 @@ class CalcViewController: UIViewController {
         self.mainButtonView.addConstraints(buttonConstraintH_4)
         self.mainButtonView.addConstraints(buttonConstraintH_5)
         self.mainButtonView.addConstraints(buttonConstraintH_6)
+    }
+    func addExtraButtonConstraints(){
+        for i in extraCalculatorButtons{
+            self.extraButtonsView.addSubview(i.calculatorButton!) //add each button in the button array to the view
+            i.calculatorButton!.translatesAutoresizingMaskIntoConstraints = false //remove auto constraints for each button in the button array
+        }
+        
+        let buttonConstraint0 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[EB0]-[EB6(==EB0)]-[EB12(==EB0)]-[EB18(==EB0)]-[EB24(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraint1 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[EB1(==EB0)]-[EB7(==EB0)]-[EB13(==EB0)]-[EB19(==EB0)]-[EB25(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraint2 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[EB2(==EB0)]-[EB8(==EB0)]-[EB14(==EB0)]-[EB20(==EB0)]-[EB26(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraint3 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[EB3(==EB0)]-[EB9(==EB0)]-[EB15(==EB0)]-[EB21(==EB0)]-[EB27(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraint4 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[EB4(==EB0)]-[EB10(==EB0)]-[EB16(==EB0)]-[EB22(==EB0)]-[EB28(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraint5 = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[EB5(==EB0)]-[EB11(==EB0)]-[EB17(==EB0)]-[EB23(==EB0)]-[EB29(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+
+        let buttonConstraintH0 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-[EB0]-[EB1(==EB0)]-[EB2(==EB0)]-[EB3(==EB0)]-[EB4(==EB0)]-[EB5(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraintH1 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-[EB6(==EB0)]-[EB7(==EB0)]-[EB8(==EB0)]-[EB9(==EB0)]-[EB10(==EB0)]-[EB11(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraintH2 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-[EB12(==EB0)]-[EB13(==EB0)]-[EB14(==EB0)]-[EB15(==EB0)]-[EB16(==EB0)]-[EB17(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraintH3 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-[EB18(==EB0)]-[EB19(==EB0)]-[EB20(==EB0)]-[EB21(==EB0)]-[EB22(==EB0)]-[EB23(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+        let buttonConstraintH4 = NSLayoutConstraint.constraints(withVisualFormat: "H:|-[EB24(==EB0)]-[EB25(==EB0)]-[EB26(==EB0)]-[EB27(==EB0)]-[EB28(==EB0)]-[EB29(==EB0)]-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: self.buttonDictionary)
+
+        self.extraButtonsView.addConstraints(buttonConstraint0)
+        self.extraButtonsView.addConstraints(buttonConstraint1)
+        self.extraButtonsView.addConstraints(buttonConstraint2)
+        self.extraButtonsView.addConstraints(buttonConstraint3)
+        self.extraButtonsView.addConstraints(buttonConstraint4)
+        self.extraButtonsView.addConstraints(buttonConstraint5)
+        self.extraButtonsView.addConstraints(buttonConstraintH0)
+        self.extraButtonsView.addConstraints(buttonConstraintH1)
+        self.extraButtonsView.addConstraints(buttonConstraintH2)
+        self.extraButtonsView.addConstraints(buttonConstraintH3)
+        self.extraButtonsView.addConstraints(buttonConstraintH4)
     }
 }
